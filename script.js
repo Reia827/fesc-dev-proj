7b886a4b3c4719456722cd8ad2a9be7275df5f53
Rachel Rawlings <rachel.0827@gmail.com>
2018-07-29 17:01:35 -0700

    board to columns

--- script.js
+++ script.js
@@ -17,3 +17,17 @@ for (i = 0; i < coll.length; i++) {
     } 
   });
 }
+
+// Get the elements with class="column"
+var elements = document.getElementsByClassName("column");
+
+// Declare a loop variable
+var i;
+
+// Grid View
+function gridView() {
+    for (i = 0; i < elements.length; i++) {
+        elements[i].style.width = "50%";
+    }
+}
+
