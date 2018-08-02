660b8f1c76be827539196a3447c38c2e4f1f31cc
Rachel Rawlings <rachel.0827@gmail.com>
2018-08-01 22:12:40 -0700

    finish html and css

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 //Home page read more read less button
 
+function myFunction() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("myBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more"; 
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less"; 
+    moreText.style.display = "inline";
+  }
+}
 
 //advisory board collapsible element
 
