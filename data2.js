b8d31947333d6b7fcbb9b27edb021f34bffeb021
Rachel Rawlings <rachel.0827@gmail.com>
2018-08-11 15:49:47 -0700

    api1

--- data2.js
+++ data2.js
@@ -1,4 +1,5 @@
 //this work is from class on August 6
+//this code added to api-page
 
 // 1 DOMContentLoaded --add event listener for this
 // 2 Google chart library loaded
@@ -6,21 +7,19 @@
 // 4 data received from API
 // 5 draw chart
 
-function onDomLoad(){
-//  console.log("I'm the first step, I should load Google library ")
+function onDOMLoad(){
+  console.log("I'm the first step, I should load Google library ")
   google.charts.load('current', {'packages':['corechart','bar']});
   google.charts.setOnLoadCallback(getData);
 }
 
 document.addEventListener("DOMContentLoaded", onDOMLoad)
 
-//start here with cheris code
-
 function getData(){
-  //console.log("Getting data is the third step! ")
+  console.log("Getting data is the third step! ")
 
   let request = new XMLHttpRequest()
-  let requestUrl = "https://api.eia.gov/series/?api_key=f13519008e6e796d59f32d03bcaa2d60&series_id=SEDS.REPRB.FL.A"
+  let requestUrl = "http://api.eia.gov/series/?api_key=f13519008e6e796d59f32d03bcaa2d60&series_id=SEDS.REPRB.FL.A"
   request.open('GET', requestUrl, true)
 
   request.onload = function(){
@@ -31,10 +30,9 @@ function getData(){
 
     let response = JSON.parse(request.response)
     console.log(response.series[0].data)
-    drawProductionChart()
+    drawProductionChart(response.series[0].data)
   }
 
-//keep error code
   request.error = function(err){
     console.log("error is: ", err)
     return
@@ -47,36 +45,14 @@ function drawProductionChart(freshData) {
   data.addColumn('string', 'Year');
   data.addColumn('number', 'Renewable Energy Production');
   data.addRows(freshData);
-}
-      //google.charts.setOnLoadCallback(drawConsumptionChart);
 
-        ]);
+  var options = {'title':'Annual renewable energy production in Florida',
+                 legend: { position: 'bottom' },
+                  vAxis: {title: "Billion Btu"},
+                 hAxis: {title: "Year"},
+                 'colors': ['#f45341']
+                };
 
-        var options = {'title':'Annual renewable energy production in Florida',
-                       legend: { position: 'bottom' },
-                        vAxis: {title: "Billion Btu"},
-                       hAxis: {title: "Year"},
-                       'colors': ['#f45341']
-                      };
-
-        var chart = new google.visualization.ColumnChart(document.getElementById('chart-production'));
-        chart.draw(data, options);
-      }
-
-      // 1 DOMContentLoaded --add event listener for this
-      // 2 Google chart library loaded
-      // 3 get data from api
-      // 4 data received from API
-      // 5 draw chart
-
-function resize () {
-   drawConsumptionChart();
-   drawProductionChart();
-  console.log("test")
-}
-if (window.addEventListener) {
-    window.addEventListener('resize', resize);
-}
-else {
-    window.attachEvent('onresize', resize);
+  var chart = new google.visualization.ColumnChart(document.getElementById('chart-production'));
+  chart.draw(data, options);
 }
